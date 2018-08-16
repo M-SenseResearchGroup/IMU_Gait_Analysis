6fe6cee7304cdd455289bb7e1c0313be50af4ae7
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-08-16 08:07:31 -0400

    Moved kalman filter and its setup into its own function

--- IMUGaitParameters/extract_gait_parameters.py
+++ IMUGaitParameters/extract_gait_parameters.py
@@ -232,6 +232,197 @@ class GaitParameters:
 
         return zed, zind.flatten()
 
+    @staticmethod
+    def _kalman_filter(t, a, w, g, zind, min_stance_time, swing_thresh):
+        """
+        Kalman filter setup and operation for pedestrian tracking
+
+        Parameters
+        ----------
+        t : array_like
+            1D array of N timestamps corresponding to acceleration and angular velocity data.
+        a : array_like
+            Nx3 array of accelerations
+        w : array_like
+            Nx3 array of angular velocities
+        g : array_like
+            1D array of gravity determined during still standing
+        zind : array_like
+            Boolean array indicating if the distance from stillness (zed) is less than the threshold
+        min_stance_time : float
+            Minimum time required for a stance to have/be occurring
+        swing_thresh : float
+            Minimum angular velocity value to be considered in the swing phase
+        """
+
+        # TODO add inputs to effect sigma and other variables set down below
+        fs = 1 / np.mean(np.diff(t))
+
+        n, a = a.shape  # get data size
+
+        state = np.ones(n)  # pre-allocate for state storage
+        state[0] = 0
+        statef = np.copy(state)  # pre-allocate for smoothed state
+
+        # calculate Euler angles assuming stationary sensor
+        pitch = np.mean(-arcsin(a[0, 0] / np.linalg.norm(g)))
+        roll = np.mean(arctan(a[0, 1] / a[0, 2]))
+        yaw = 0
+
+        # Rotation matrix?
+        C = np.array([[cos(pitch), sin(roll) * sin(pitch), cos(roll) * cos(pitch)],
+                      [0, cos(roll), -sin(roll)],
+                      [-sin(pitch), sin(roll) * cos(pitch), cos(roll) * cos(pitch)]])
+
+        C_prev = C.copy()
+
+        # pre-allocate storage for heading direction.  Indicates direction subject is facing, and not
+        # necessarily the direction of travel
+        heading = np.zeros(n)
+        heading[0] = yaw
+
+        # pre-allocate storage for acceleration in navigational frame
+        acc_n = np.zeros((3, n))
+        acc_n[:, 0] = C @ a[0, :]
+
+        # pre-allocate storage for velocity in navigational frame, with initial velocity assumed to be 0
+        vel_n = np.zeros((3, n))
+        vel_n[:, 0] = np.array([0, 0, 0])
+
+        # pre-allocate storage for position in navigational frame.  Initial position arbitrarily set to 0
+        pos_n = np.zeros((3, n))
+        pos_n[:, 0] = np.array([0, 0, 0])
+
+        # pre-allocate storage for distance traveled used for altitude plots
+        distance = np.zeros(n)
+        distance[0] = 0
+
+        # Error covariance matrix
+        P = np.zeros((9, 9))
+
+        # process noise parameter, gyroscope and accelerometer noise
+        sigma_omega = 1e-2
+        sigma_a = 1e-2
+
+        # ZUPT measurement matrix
+        H = np.concatenate((np.zeros((3, 3)), np.zeros((3, 3)), np.identity(3)), axis=1)
+
+        # ZUPT measurement noise covariance matrix
+        sigma_v = 1e-3
+        R = np.diag([sigma_v] * 3) ** 2
+
+        wmax = 0
+        start_swing = 0
+        start_stance = 0
+
+        # Main loop for Kalman Filter
+        for i in range(1, n):
+            # START INS (transformation, double integration)
+            dt = t[i] - t[i-1]  # seconds
+
+            # skew symmetric matrix for angular rates
+            wm = np.array([[0, -w[i, 2], w[i, 1]],
+                           [w[i, 2], 0, -w[i, 0]],
+                           [-w[i, 1], w[i, 0], 0]])
+
+            # TODO check lstsq result
+            # orientation estimation
+            C = C_prev @ np.linalg.lstsq(2 * np.identity(3) + wm * dt, 2 * np.identity(3) - wm * dt, rcond=None)[0]
+
+            # transform the acceleration into the navigational frame
+            acc_n[:, i] = 0.5 * (C + C_prev) @ a[i, :]
+
+            # velocity and position using trapezoidal integration
+            gz = np.array([0, 0, np.linalg.norm(g)])
+            vel_n[:, i] = vel_n[:, i - 1] + ((acc_n[:, i] - gz) + (acc_n[:, i - 1] - gz)) * dt / 2
+            pos_n[:, i] = pos_n[:, i - 1] + (vel_n[:, i] + vel_n[:, i - 1]) * dt / 2
+
+            # skew symmetric matrix from navigational frame acceleration
+            S = np.array([[0, -acc_n[2, i], acc_n[1, i]],
+                          [acc_n[2, i], 0, -acc_n[0, i]],
+                          [-acc_n[1, i], acc_n[0, i], 0]])
+
+            # create state transition matrix
+            _r1 = np.concatenate((np.identity(3), np.zeros((3, 3)), np.zeros((3, 3))), axis=1)
+            _r2 = np.concatenate((np.zeros((3, 3)), np.identity(3), dt * np.identity(3)), axis=1)
+            _r3 = np.concatenate((-dt * S, np.zeros((3, 3)), np.identity(3)), axis=1)
+
+            F = np.concatenate((_r1, _r2, _r3), axis=0)
+
+            # compute the process noise covariance
+            Q = np.diag([sigma_omega, sigma_omega, sigma_omega, 0, 0, 0, sigma_a, sigma_a, sigma_a]) ** 2
+
+            # propagate the error covariance matrix
+            P = F @ P @ np.linalg.inv(F) + Q
+
+            # END of INS
+
+            # stance phase detection and zero-velocity updates
+            if zind[i]:
+                # START Kalman Filter zero-velocity updates
+                state[i] = 0  # stance
+
+                # TODO check lstsq result
+                # Kalman Gain
+                K, _, _, _ = np.linalg.lstsq((P @ H.transpose()).T, H @ P @ H.transpose() + R, rcond=None)
+
+                # update the filter state
+                delta_x = K @ vel_n[:, i]
+
+                # update the error covariance matrix
+                P = (np.identity(9) - K @ H) @ P  # simplified covariance update found in most books
+
+                # extract errors from the KF states
+                attitude_e = delta_x[:3]
+                pos_e = delta_x[3:6]
+                vel_e = delta_x[6:]
+                # END Kalman Filter zero-velocity update
+
+                # APPLY corrections to INS estimates
+                # skew symmetric matrix for small angles to correct orientation
+                ang_mat = -np.array([[0, -attitude_e[2], attitude_e[1]],
+                                     [attitude_e[2], 0, -attitude_e[0]],
+                                     [-attitude_e[1], attitude_e[0], 0]])
+
+                # TODO check lstsq result
+                # correct orientation
+                C = np.linalg.lstsq(2 * np.identity(3) + ang_mat, 2 * np.identity(3) - ang_mat, rcond=None)[0] @ C
+
+                # correct position and velocity estimates
+                vel_n[:, i] -= vel_e
+                pos_n[:, i] -= pos_e
+
+            # smooth states
+            statef[i] = state[i]
+
+            if state[i] - state[i - 1] == 1:  # 0 (stance) -> 1 (swing)
+                # deal with false positive stance
+                if (i - start_stance) / fs <= min_stance_time:
+                    statef[start_stance:i] = 1
+                start_swing = i
+                wmax = 0
+
+            if np.linalg.norm(w[i, :]) > wmax:
+                wmax = np.linalg.norm(w[i, :])
+
+            if state[i] - state[i - 1] == -1:  # 1 (swing) -> 0 (stance)
+                # deal with false positive
+                if wmax < swing_thresh:
+                    statef[start_swing:i] = 0
+                start_stance = i
+
+            # Estimate and save the yaw of the sensor (different than direction of travel)
+            # unused here but potentially useful for orienting a GUI
+            heading[i] = arctan2(C[1, 0], C[0, 0])
+            C_prev = C.copy()  # Save orientation estimate, required at start of main loop
+
+            # compute horizontal distance
+            distance[i] = distance[i - 1] + np.sqrt((pos_n[0, i] - pos_n[0, i - 1]) ** 2 +
+                                                    (pos_n[1, i] - pos_n[0, i - 1]) ** 2)
+
+        return acc_n, vel_n, pos_n, state, statef
+
+
     def process_data(self):
         """
         Process the raw data and run it through a Kalman Filter to obtain heading and other signals
@@ -254,169 +445,7 @@ class GaitParameters:
                                                                            self.raw_data[s][l]['gyro'][e][:, 1:],
                                                                            self.g[s][l], self.zt)
 
-                    fs = 1/np.mean(np.diff(self.raw_data[s][l]['accel'][e][:, 0]))
 
-                    n, a = self.raw_data[s][l]['accel'][e][:, 1:].shape  # get data size
-
-                    state = np.ones(n)  # pre-allocate for state storage
-                    state[0] = 0
-                    statef = np.copy(state)  # pre-allocate for smoothed state
-
-                    # calculate Euler angles assuming stationary sensor
-                    pitch = np.mean(-arcsin(self.raw_data[s][l]['accel'][e][0, 1]/np.linalg.norm(self.g[s][l])))
-                    roll = np.mean(arctan(self.raw_data[s][l]['accel'][e][0, 2]/self.raw_data[s][l]['accel'][e][0, 3]))
-                    yaw = 0
-
-                    # Rotation matrix?
-                    C = np.array([[cos(pitch), sin(roll)*sin(pitch), cos(roll)*cos(pitch)],
-                                  [0, cos(roll), -sin(roll)],
-                                  [-sin(pitch), sin(roll)*cos(pitch), cos(roll)*cos(pitch)]])
-
-                    C_prev = C.copy()
-
-                    # pre-allocate storage for heading direction.  Indicates direction subject is facing, and not
-                    # necessarily the direction of travel
-                    heading = np.zeros(n)
-                    heading[0] = yaw
-
-                    # pre-allocate storage for acceleration in navigational frame
-                    acc_n = np.zeros((3, n))
-                    acc_n[:, 0] = C @ self.raw_data[s][l]['accel'][e][0, 1:]
-
-                    # pre-allocate storage for velocity in navigational frame, with initial velocity assumed to be 0
-                    vel_n = np.zeros((3, n))
-                    vel_n[:, 0] = np.array([0, 0, 0])
-
-                    # pre-allocate storage for position in navigational frame.  Initial position arbitrarily set to 0
-                    pos_n = np.zeros((3, n))
-                    pos_n[:, 0] = np.array([0, 0, 0])
-
-                    # pre-allocate storage for distance traveled used for altitude plots
-                    distance = np.zeros(n)
-                    distance[0] = 0
-
-                    # Error covariance matrix
-                    P = np.zeros((9, 9))
-
-                    # process noise parameter, gyroscope and accelerometer noise
-                    sigma_omega = 1e-2
-                    sigma_a = 1e-2
-
-                    # ZUPT measurement matrix
-                    H = np.concatenate((np.zeros((3, 3)), np.zeros((3, 3)), np.identity(3)), axis=1)
-
-                    # ZUPT measurement noise covariance matrix
-                    sigma_v = 1e-3
-                    R = np.diag([sigma_v]*3)**2
-
-                    wmax = 0
-                    start_swing = 0
-                    start_stance = 0
-
-                    # Main loop for Kalman Filter
-                    for i in range(1, n):
-                        # START INS (transformation, double integration)
-                        dt = self.raw_data[s][l]['accel'][e][i, 0] - self.raw_data[s][l]['accel'][e][i-1, 0]  # seconds
-
-                        # skew symmetric matrix for angular rates
-                        wm = np.array([[0, -self.raw_data[s][l]['gyro'][e][i, 3], self.raw_data[s][l]['gyro'][e][i, 2]],
-                                       [self.raw_data[s][l]['gyro'][e][i, 3], 0, -self.raw_data[s][l]['gyro'][e][i, 1]],
-                                       [-self.raw_data[s][l]['gyro'][e][i, 2], self.raw_data[s][l]['gyro'][e][i, 1], 0]])
-
-                        # TODO check lstsq result
-                        # orientation estimation
-                        C = C_prev @ np.linalg.lstsq(2*np.identity(3) + wm*dt, 2*np.identity(3) - wm*dt, rcond=None)[0]
-
-                        # transform the acceleration into the navigational frame
-                        acc_n[:, i] = 0.5*(C + C_prev) @ self.raw_data[s][l]['accel'][e][i, 1:]
-
-                        # velocity and position using trapezoidal integration
-                        gz = np.array([0, 0, np.linalg.norm(self.g[s][l])])
-                        vel_n[:, i] = vel_n[:, i-1] + ((acc_n[:, i] - gz) + (acc_n[:, i-1] - gz))*dt/2
-                        pos_n[:, i] = pos_n[:, i-1] + (vel_n[:, i] + vel_n[:, i-1])*dt/2
-
-                        # skew symmetric matrix from navigational frame acceleration
-                        S = np.array([[0, -acc_n[2, i], acc_n[1, i]],
-                                      [acc_n[2, i], 0, -acc_n[0, i]],
-                                      [-acc_n[1, i], acc_n[0, i], 0]])
-
-                        # create state transition matrix
-                        _r1 = np.concatenate((np.identity(3), np.zeros((3, 3)), np.zeros((3, 3))), axis=1)
-                        _r2 = np.concatenate((np.zeros((3, 3)), np.identity(3), dt*np.identity(3)), axis=1)
-                        _r3 = np.concatenate((-dt*S, np.zeros((3, 3)), np.identity(3)), axis=1)
-
-                        F = np.concatenate((_r1, _r2, _r3), axis=0)
-
-                        # compute the process noise covariance
-                        Q = np.diag([sigma_omega, sigma_omega, sigma_omega, 0, 0, 0, sigma_a, sigma_a, sigma_a])**2
-
-                        # propagate the error covariance matrix
-                        P = F @ P @ np.linalg.inv(F) + Q
-
-                        # END of INS
-
-                        # stance phase detection and zero-velocity updates
-                        if zind[i]:
-                            # START Kalman Filter zero-velocity updates
-                            state[i] = 0  # stance
-
-                            # TODO check lstsq result
-                            # Kalman Gain
-                            K, _, _, _ = np.linalg.lstsq((P @ H.transpose()).T, H @ P @ H.transpose() + R, rcond=None)
-
-                            # update the filter state
-                            delta_x = K @ vel_n[:, i]
-
-                            # update the error covariance matrix
-                            P = (np.identity(9) - K @ H) @ P  # simplified covariance update found in most books
-
-                            # extract errors from the KF states
-                            attitude_e = delta_x[:3]
-                            pos_e = delta_x[3:6]
-                            vel_e = delta_x[6:]
-                            # END Kalman Filter zero-velocity update
-
-                            # APPLY corrections to INS estimates
-                            # skew symmetric matrix for small angles to correct orientation
-                            ang_mat = -np.array([[0, -attitude_e[2], attitude_e[1]],
-                                                 [attitude_e[2], 0, -attitude_e[0]],
-                                                 [-attitude_e[1], attitude_e[0], 0]])
-
-                            # TODO check lstsq result
-                            # correct orientation
-                            C = np.linalg.lstsq(2*np.identity(3)+ang_mat, 2*np.identity(3)-ang_mat, rcond=None)[0] @ C
-
-                            # correct position and velocity estimates
-                            vel_n[:, i] -= vel_e
-                            pos_n[:, i] -= pos_e
-
-                        # smooth states
-                        statef[i] = state[i]
-
-                        if state[i]-state[i-1] == 1:  # 0 (stance) -> 1 (swing)
-                            # deal with false positive stance
-                            if (i - start_stance)/fs <= self.mst:
-                                statef[start_stance:i] = 1
-                            start_swing = i
-                            wmax = 0
-
-                        if np.linalg.norm(self.raw_data[s][l]['gyro'][e][i, :]) > wmax:
-                            wmax = np.linalg.norm(self.raw_data[s][l]['gyro'][e][i, :])
-
-                        if state[i]-state[i-1] == -1:  # 1 (swing) -> 0 (stance)
-                            # deal with false positive
-                            if wmax < self.swt:
-                                statef[start_swing:i] = 0
-                            start_stance = i
-
-                        # Estimate and save the yaw of the sensor (different than direction of travel)
-                        # unused here but potentially useful for orienting a GUI
-                        heading[i] = arctan2(C[1, 0], C[0, 0])
-                        C_prev = C.copy()  # Save orientation estimate, required at start of main loop
-
-                        # compute horizontal distance
-                        distance[i] = distance[i-1] + np.sqrt((pos_n[0, i]-pos_n[0, i-1])**2 +
-                                                              (pos_n[1, i]-pos_n[0, i-1])**2)
 
 
 
